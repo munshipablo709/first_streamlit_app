6ddf951dab342863878eb92fbce5b7754fc5be06
munshipablo709 <138006071+munshipablo709@users.noreply.github.com>
2023-07-03 13:09:46 +0300

    coorected quotes in  food addtion box

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,6 @@ streamlit.dataframe(my_data_rows)
 
 
 # fruit addition box
-fruit_choice = streamlit.text_input('What fruit would you like to add'?','Jackfruit')
+fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
 
